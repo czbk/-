c23d70de513d3cfaa016c3ba40932327f30c85a6
pyawkk <624204727@qq.com>
2014-08-26 23:02:35 +0800

    随便添加了几行代码

--- 蔷薇岛屿/AppDelegate.m
+++ 蔷薇岛屿/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainViewController.h"
 
 @implementation AppDelegate
 
@@ -15,6 +16,9 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    MainViewController *main = [[MainViewController alloc] init];
+    self.window.rootViewController = main ;
+    main.view.backgroundColor = [UIColor grayColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
