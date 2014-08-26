3355e12208bb80a2d93b54f37a7aee728073446e
pyawkk <624204727@qq.com>
2014-08-26 22:43:58 +0800

    Initial Commit

---
+++ 蔷薇岛屿/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  蔷薇岛屿
+//
+//  Created by panyong on 14-8-26.
+//  Copyright (c) 2014年 pyagby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 蔷薇岛屿Tests/____Tests.m
@@ -0,0 +1,34 @@
+//
+//  ____Tests.m
+//  蔷薇岛屿Tests
+//
+//  Created by panyong on 14-8-26.
+//  Copyright (c) 2014年 pyagby. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ____Tests : XCTestCase
+
+@end
+
+@implementation ____Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
